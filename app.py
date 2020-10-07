19327629297505da416608438a5026a1ba7e889d
sidraj000 <siddharthraj000@gmail.com>
2020-10-07 13:54:06 +0530

    backend working

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS,cross_origin
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8529454669@localhost:5432/dummy"
 db = SQLAlchemy(app)
@@ -23,34 +24,48 @@ class SavingModel(db.Model):
     def __repr__(self):
         return f"<user {self.userName}>"
   
+@app.route('/total',methods=['GET'])
+def toal_users():
+     userSavings = SavingModel.query.all()
+     return {"totalUsers":len(userSavings)}
 
 @app.route('/savings', methods=['POST'])
-@cross_origin(allow_headers=['Content-Type'])
-def handle_cars():
-     data = request.get_json()
-    #dataName=data['userName']
-     dataSavings=(data['How much percent of salary do you save?'])
-    # dataSalary=data['What is your yearly income?']
-    #dataSalary=request.args.get('What is your yearly income?')
-    #  userSavings = SavingModel(userName="unknown", savings=dataSavings, salary=dataSalary)
-    #  db.session.add(userSavings)
-    #  db.session.commit()
-    #  userSavings = SavingModel.query.filter_by(salary = dataSalary).all()
-    #  s1 = [0]*10
-    #  results = [
-    #         {
-    #             "userName": userSaving.userName,
-    #             "savings": userSaving.savings,
-    #             "salary": userSaving.salary
-    #         } for userSaving in userSavings]
-    #  percentile=0
-    #  total=len(userSavings)
-    #  smaller=0
-    #  for userSaving in userSavings:
-    #      if (userSaving.savings < dataSavings):
-    #          smaller=smaller+1
-    #  percentile=(smaller/total*100)
-     return ({'dataSavings': 'sid'})
+def handle_submission():
+     data =  request.get_json(force=True)
+     dataSavings=int(data['How much percent of salary do you save?'])
+     dataSalary=data['What is your yearly income?']
+     dataName=data['userName']
+
+     currentUsersData=SavingModel.query.filter_by(userName = dataName).count()
+     print(currentUsersData)
+     if (currentUsersData)==0:
+      userSavings = SavingModel(userName=dataName, savings=dataSavings, salary=dataSalary)
+      db.session.add(userSavings)
+      db.session.commit()
+
+     userSavings = SavingModel.query.filter_by(salary = dataSalary).all()
+     s1 = [0]*10
+     results = [
+            {
+                "userName": userSaving.userName,
+                "savings": userSaving.savings,
+                "salary": userSaving.salary
+            } for userSaving in userSavings]
+     percentile=0
+     total=len(userSavings)
+     smaller=0 
+     for userSaving in userSavings:
+         if (userSaving.savings < dataSavings):
+             smaller=smaller+1
+             
+     percentile=(smaller/total*100)
+     
+     #rounding to two decimal places
+     percentile=percentile*100
+     percentile=int(percentile)
+     percentile=float(percentile/100)
+
+     return {"Percentile":percentile}
     
 
 if __name__ == '__main__':
